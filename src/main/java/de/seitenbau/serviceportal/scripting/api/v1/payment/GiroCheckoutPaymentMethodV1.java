a98361019f2936987428eb755d6e1d46a9684627
seitenbau <no-reply@seitenbau.com>
2024-01-22 11:58:58 +0000

    Publish Version: 1.171.8

--- src/main/java/de/seitenbau/serviceportal/scripting/api/v1/payment/GiroCheckoutPaymentMethodV1.java
+++ src/main/java/de/seitenbau/serviceportal/scripting/api/v1/payment/GiroCheckoutPaymentMethodV1.java
@@ -8,7 +8,11 @@ public enum GiroCheckoutPaymentMethodV1
   /** Antragssteller wählt Bezahlverfahren auf externer Payment-Page aus. */
   PAYMENTPAGE,
 
-  /** Paydirekt. */
+  /**
+   * paydirekt. Wurde zum 01.01.2024 mit giropay zusammengelegt und wird nicht mehr unterstützt.
+   * Stattdessen {@link #GIROPAY} nutzen.
+   */
+  @Deprecated(since = "1.171")
   PAYDIREKT,
 
   /** giropay. */
